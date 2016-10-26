47672e19556f63257ba329c91170b1ec6c98eb95
boon1502 <boon1502@student.nhl.nl>
2016-10-26 10:52:02 +0200

    ModelLoader testing

--- js/Main.js
+++ js/Main.js
@@ -1,8 +1,9 @@
 /**
  * Created by annepieter on 24/10/2016.
  */
-var scene, camera, directionalLight, renderer, controls;
+var scene, camera, directionalLight, renderer, controls, ambientlight;
 var material,geometry, cube;
+var loader, mesh;
 init();
 animate();
 function init(){
@@ -13,17 +14,25 @@ function init(){
     directionalLight = new THREE.DirectionalLight(0xE5E5E5,1);
     directionalLight.position.set( camera.position.x,camera.position.y,camera.position.z );
     directionalLight.target.position.set(0,0,0);
+    ambientlight = new THREE.AmbientLight(0xE5E5E5,1);
+
     scene.add(directionalLight);
 
     material = new THREE.MeshPhongMaterial({color: 0x123456});
-    geometry = new THREE.BoxGeometry(1,1,1);
+    /*geometry = new THREE.BoxGeometry(1,1,1);
     cube = new THREE.Mesh(geometry,material);
     cube.position.set(0,0,0);
-    scene.add(cube);
+    scene.add(cube);*/
+
+    loader = new THREE.JSONLoader();
+    loader.load('js/MSI.json', function(MSI){
+        mesh = new THREE.Mesh(MSI, material);
+        scene.add(mesh);
+    });
 
 
     renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth - 20, window.innerHeight - 20);
+    renderer.setSize(window.innerWidth - 200, window.innerHeight - 200);
     renderer.setClearColor(0xB2DFEE);
     document.body.appendChild(renderer.domElement);
     controls = new THREE.OrbitControls(camera);
@@ -39,3 +48,4 @@ function animate(){
     requestAnimationFrame(animate);
 
 }
+animate();
