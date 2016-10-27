cfb40b43ead73264d345d19fe77e28ef46f4c1d2
Sam de Bruin <Sam de Bruin>
2016-10-27 12:56:10 +0200

    Added Sidepanels, changed path in objectinit

--- objectinit.js
+++ objectinit.js
@@ -2,11 +2,11 @@
  * Created by annepieter on 27/10/2016.
  */
 var mtlLoader = new THREE.MTLLoader();
-mtlLoader.setPath( 'js/' );
-mtlLoader.load( 'MSI.mtl', function( materials1 ) {
+mtlLoader.setPath( 'models/ThreeJS/' );
+mtlLoader.load( 'windowSide.mtl', function( materials1 ) {
     materials1.preload();
     var loader = new THREE.OBJLoader();
     loader.setMaterials(materials1);
-    loader.load( 'js/MSI.obj', function ( object ) {
+    loader.load( 'models/ThreeJS/windowSide.obj', function ( object ) {
         scene.add( object );
     } );});
