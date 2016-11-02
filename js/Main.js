bd5d57d504d7c093e217624c101745a66599413f
boon1502 <boon1502@student.nhl.nl>
2016-11-02 15:12:34 +0100

    color change + hdd rack

--- js/Main.js
+++ js/Main.js
@@ -6,7 +6,7 @@ var material, geometry, cube, material2;
 var objarr = [];
 var jsonarr =[];
 var mtlLoader = new THREE.MTLLoader();
-mtlLoader.setPath( 'models/ThreeJs/Stock/' );
+
 init();
 animate();
 function init(){
@@ -18,9 +18,12 @@ function init(){
     directionalLight.position.set( camera.position.x,camera.position.y,camera.position.z );
     directionalLight.target.position.set(0,0,0);
     var ambientlight = new THREE.AmbientLight(0xffffff, 0.5);
+    var pointLight = new THREE.SpotLight(0xff0000,1,0,0.5);
+    pointLight.position.set(0,2,0);
+    pointLight.castShadow = true;
 
 
-    scene.add(directionalLight,ambientlight);
+    scene.add(directionalLight,ambientlight, pointLight);
 
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth - 200, window.innerHeight - 200);
@@ -29,7 +32,7 @@ function init(){
     controls = new THREE.OrbitControls(camera);
     controls.addEventListener( 'change' );
     material = new THREE.MeshPhongMaterial({color:0xffffff});
-    material2 = new THREE.MeshPhongMaterial({color:0x000000});
+    material2 = new THREE.MeshPhongMaterial({color:0x343434});
     animate();
 
 }
@@ -50,6 +53,14 @@ function ColorHex(){
     var color = prompt("give color hex 0x......");
     if (color != null && color.toString().length == 6) {
         material.color.setHex('0x'+color);
+    }
+    else {alert("beter geef je groter nummer dan");ColorHex()}
+}
+
+function ColorHex2(){
+    var color = prompt("give color hex 0x......");
+    if (color != null && color.toString().length == 6) {
+        material2.color.setHex('0x'+color);
     }
     else {alert("beter geef je groter nummer dan");ColorHex()}
 }
@@ -77,11 +88,16 @@ function FetchModels() {
     }
 }
 function ShowModel() {
+
+
     var x = document.getElementById("sidepanels");
     var modelToLoad = x.options[x.selectedIndex].value;
     var modelColor = modelToLoad.split('#');
     var modelColor2 = modelColor[1].split('*');
+    material.color.setHex('0x' + modelColor2[0]);
     material2.color.setHex('0x' + modelColor2[1]);
+    console.log(modelColor[1]);
+    console.log(material);
     if (x.selectedIndex <  jsonarr.length) {
         var jsonloader = new THREE.ObjectLoader();
         jsonloader.load('models/ThreeJS/logos/'+ modelColor[0] + '.json', function (frontTexture) {
@@ -93,10 +109,13 @@ function ShowModel() {
             frontTexture.position.x = 1.95;
             frontTexture.position.y = 2.05;
             frontTexture.rotation.y = Math.PI;
+            scene.remove(scene.children[9]);
             scene.add(frontTexture);
+            console.log(scene.children);
         });
     }
     else {
+        mtlLoader.setPath( 'models/ThreeJs/logos/' );
         var modelColor = modelToLoad.split('#');
         var modelColor2 = modelColor[1].split('*');
         material.color.setHex('0x' + modelColor[1]);
@@ -104,11 +123,12 @@ function ShowModel() {
 
             Selected.preload();
             var loader = new THREE.OBJLoader();
-            loader.setPath('models/ThreeJs/Stock/');
+            loader.setPath('models/ThreeJs/logos/');
             loader.setMaterials(Selected);
             loader.load(modelColor[0] + '.obj', function (object) {
                 object.position.x = 1.95;
                 object.position.y = 2.05;
+                scene.remove(scene.children[9]);
                 scene.add(object);
 
             });
--- objectinit.js
+++ objectinit.js
@@ -5,128 +5,17 @@
 
 /** Side panels **/
 function Frontpanelpush(){
-    jsonarr.push("alienware#000000*20ea19");
-    jsonarr.push("aperture#FFFFFF*adaeff");
-    jsonarr.push("MSI#000000*FF0000");
-    jsonarr.push("nvidia#000000*48a11a");
+    jsonarr.push("alienware#343434*20ea19");
+    jsonarr.push("Aperture#FFFFFF*adaeff");
+    jsonarr.push("MSI#343434*FF0000");
+    jsonarr.push("nvidia#343434*48a11a");
     jsonarr.push("VS#FFFFFF*551A8B");
-    jsonarr.push("Front1#000000*FF0000");
+    jsonarr.push("Front1#343434*FF0000");
+    objarr.push("razer#343434");
+    objarr.push("alliance#1d3464");
     objarr.push("deadpool#010101");
     objarr.push("Overwatch#FF4800");
 }
-/*
-mtlLoader.load( 'CaseTest.mtl', function( windowSide ) {
-    windowSide.preload();
-    var loader = new THREE.OBJLoader();
-    loader.setPath( 'models/ThreeJs/Stock/' );
-    loader.setMaterials(windowSide);
-    loader.load( 'CaseTest.obj', function ( object ) {
-        scene.add( object );
-    } );});
-
-mtlLoader.load( 'windowlessSide.mtl', function( windowless ) {
-    windowless.preload();
-    var loader = new THREE.OBJLoader();
-    loader.setPath( 'models/ThreeJs/Stock/' );
-    loader.setMaterials(windowless);
-    loader.load( 'windowlessSide.obj', function ( object ) {
-        scene.add( object );
-    } );});*/
-
-    /** Front panels **/
-
-/*mtlLoader.load( 'alienware.mtl', function( alienware ) {
-
-    alienware.preload();
-    var loader = new THREE.OBJLoader();
-    loader.setPath( 'models/ThreeJs/Stock/' );
-    loader.setMaterials(alienware);
-    loader.load( 'alienware.obj', function ( object ) {
-        object.visible = false;
-        scene.add( object);
-    } );});
-
-mtlLoader.load( 'Aperture.mtl', function( aperture ) {
-    aperture.preload();
-    var loader = new THREE.OBJLoader();
-    loader.setPath( 'models/ThreeJs/Stock/' );
-    loader.setMaterials(aperture);
-    loader.load( 'Aperture.obj', function ( object ) {
-        object.visible = false;
-        scene.add( object );
-    } );});
-
-mtlLoader.load( 'deadpool.mtl', function( deadpool ) {
-    deadpool.preload();
-    var loader = new THREE.OBJLoader();
-    loader.setPath( 'models/ThreeJs/Stock/' );
-    loader.setMaterials(deadpool);
-    loader.load( 'deadpool.obj', function ( object ) {
-        object.visible = false;
-        scene.add( object );
-    } );});
-
-mtlLoader.load( 'leeg.mtl', function( leeg ) {
-    leeg.preload();
-    var loader = new THREE.OBJLoader();
-    loader.setPath( 'models/ThreeJs/Stock/' );
-    loader.setMaterials(leeg);
-    loader.load( 'leeg.obj', function ( object ) {
-        object.visible = false;
-        scene.add( object );
-    } );});
-
-mtlLoader.load( 'MSI.mtl', function( MSI ) {
-    MSI.preload();
-    var loader = new THREE.OBJLoader();
-    loader.setPath( 'models/ThreeJs/Stock/' );
-    loader.setMaterials(MSI);
-    loader.load( 'MSI.obj', function ( object ) {
-        object.visible = false;
-        scene.add( object );
-    } );});
-
-mtlLoader.load( 'nvidia.mtl', function( nvidia ) {
-    nvidia.preload();
-    var loader = new THREE.OBJLoader();
-    loader.setPath( 'models/ThreeJs/Stock/' );
-    loader.setMaterials(nvidia);
-    loader.load( 'nvidia.obj', function ( object ) {
-        object.visible = false;
-        scene.add( object );
-    } );});
-
-    mtlLoader.load('Overwatch.mtl', function (Overwatch) {
-        Overwatch.preload();
-        var loader = new THREE.OBJLoader();
-        loader.setPath('models/ThreeJs/Stock/');
-        loader.setMaterials(Overwatch);
-        loader.load('Overwatch.obj', function (object) {
-            object.visible = false;
-            scene.add(object);
-        });
-    });
-
-
-mtlLoader.load( 'VSWit.mtl', function( VSWit ) {
-    VSWit.preload();
-    var loader = new THREE.OBJLoader();
-    loader.setPath( 'models/ThreeJs/Stock/' );
-    loader.setMaterials(VSWit);
-    loader.load( 'VSWit.obj', function ( object ) {
-        object.visible = false;
-        scene.add( object );
-    } );});
-
-mtlLoader.load( 'VSZwart.mtl', function( VSZwart ) {
-    VSZwart.preload();
-    var loader = new THREE.OBJLoader();
-    loader.setPath( 'models/ThreeJs/Stock/' );
-    loader.setMaterials(VSZwart);
-    loader.load( 'VSZwart.obj', function ( object ) {
-        object.visible = false;
-        scene.add( object );
-    } );});*/
 
 /** custom **/
 
@@ -162,6 +51,17 @@ jsonloader.load('models/ThreeJS/Customizable/Back.json', function(back){
     back.position.x = -1.95;
     back.position.y = 2.05;
     scene.add(back);
+});
+jsonloader.load('models/ThreeJS/Customizable/leeg.json', function(front){
+    front.traverse((function (child) {
+        if(child instanceof THREE.Mesh)
+        {
+            child.material = material;
+        }
+    }));
+    front.position.x = 1.95;
+    front.position.y = 2.05;
+    scene.add(front);
 });
 
 jsonloader.load('models/ThreeJS/Customizable/window.json', function(window){
@@ -200,9 +100,14 @@ jsonloader.load('models/ThreeJS/Customizable/windowless.json', function(windowle
     windowless.position.y = 2.05;
     scene.add(windowless);
 });
-
-
-//var jsonloader = new THREE.ObjectLoader();
-  //  var mesh = jsonloader.load('models/ThreeJS/Customizable/Front1.json');
- //   scene.add(mesh);
-
+jsonloader.load('models/ThreeJS/inside/HddRack.json', function(hdd){
+    hdd.traverse((function (child) {
+        if(child instanceof THREE.Mesh)
+        {
+            child.material = new THREE.MeshPhongMaterial({color:0xE3E3E3});
+        }
+    }));
+    hdd.position.x = 1;
+    hdd.position.y = 0.8325;
+    scene.add(hdd);
+});
